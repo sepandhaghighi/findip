a00e6f221a32b7fda5d9c01fb166d7e43eb9ceee
= <sepand.haghighi@yahoo.com>
2016-02-14 00:37:55 +0330

    feat : speed up pinging by multiprocessing and pool

--- ip_finder.py
+++ ip_finder.py
@@ -3,6 +3,10 @@ import subprocess as sub # Added For Run Command By CMD
 import string
 import datetime
 import sys # For Exit
+import multiprocessing as mu # for multtiprocessing
+def ping(i): # ping function
+    output=str(list(sub.Popen("ping "+"192.168.1."+str(i),stdout=sub.PIPE,stderr=sub.PIPE,shell=True).communicate())[0])
+    return output
 def ip_filter(i_list): # This Function Get A List Of IPs and Split IP
     '''((list)->list'''
     temp_list=[]
@@ -24,21 +28,23 @@ def search_ip(output): # This Function Get A String As Input ( ARP Command Outpu
         else:
             index=index+16
     return ip_list
-def find(mask="192.168.166.",mode="manual",iplist=[],range_min=1,range_max=255): # This Function Ping And SSH IPs to find SSH Server
+def find(mask="192.168.1.",mode="manual",iplist=[],range_min=0,range_max=254): # This Function Ping And SSH IPs to find SSH Server
     log_file=open("log_file.txt","a")
     if mode=="manual":
         try:
-            for i in range(range_min,range_max):
-                output=str(list(sub.Popen("ping "+mask+str(i),stdout=sub.PIPE,stderr=sub.PIPE,shell=True).communicate())[0])
+            ip_list=[]
+            p=mu.Pool(100) # for multiprocessing
+            result=p.map(ping,list(range(range_min,range_max))) # result of pings
+            for output in result:
                 if output.find("timed out")==-1 and output.find("unreachable")==-1:
-                    ssh_response=sub.call("ssh "+mask+str(i),stdout=sub.PIPE,stderr=sub.PIPE,timeout=30,shell=True)
-                    print("IP : ",mask+str(i),"Is available but it is not ssh server")
+                    ssh_response=sub.call("ssh "+mask+str(result.index(output)+range_min),stdout=sub.PIPE,stderr=sub.PIPE,timeout=30,shell=True)
+                    print("IP : ",mask+str(result.index(output)+range_min),"Is available but it is not ssh server")
                 else:
-                    print("IP : ",mask+str(i),"Is not available")
+                    print("IP : ",mask+str(result.index(output)+range_min),"Is not available")
             input("Press Any Key To Exit")
         except sub.TimeoutExpired:
-            print("IP : ",mask+str(i),"Is SSH Server")
-            log_file.write("IP : "+mask+str(i)+"Is SSH Server  "+str(datetime.datetime.today())+"\n")
+            print("IP : ",mask+str(result.index(output)+range_min),"Is SSH Server")
+            log_file.write("IP : "+mask+str(result.index(output)+range_min)+"Is SSH Server  "+str(datetime.datetime.today())+"\n")
             log_file.close()
             input("Press Any Key To Exit")
     else:
@@ -62,10 +68,12 @@ if __name__=="__main__":
         sys.exit()
     ssh_test=sub.Popen("ssh",stdout=sub.PIPE,stderr=sub.PIPE,shell=True)
     ssh_result=str(list(ssh_test.communicate())[1])
+    #print(ssh_result)
     if ssh_result.find("is not recognized")!=-1:
         print("Please First Install Open SSH")
         sys.exit()
     inp=int(input("Please Choose ARP[1] or Linear Search[2]"))
+    print("Please Wait : ")
     if inp==1:
         sub.Popen("ping "+my_ip,stdout=sub.PIPE ,stderr=sub.PIPE,shell=True)
         response=sub.Popen("arp -a",stdout=sub.PIPE ,stderr=sub.PIPE,shell=True)
@@ -74,21 +82,22 @@ if __name__=="__main__":
         ip_list=ip_filter(ip_list)
         find(mode="ARP",iplist=ip_list)
     else:
-        get_mask=input("Please Enter Mask :")
-        range_max_input=int(input("Please Enter Range Max : "))
-        range_min_input=int(input("Please Enter Range Min : "))
-        if get_mask.find("192.")!=-1 and get_mask.find("168.")!=-1:
-            if get_mask[-1]!=".":
-                get_mask=get_mask+"."
-            if range_max_input>range_min_input and range_max_input<256:
-                find(mask=get_mask,mode="manual",range_max=range_max_input,range_min=range_min_input)
-            else:
-                find(mask=get_mask,mode="manual")
-        else:
-            if range_max_input>range_min_input and range_max_input<256:
-                find(range_max=range_max_input,mode="manual",range_min=range_min_input)
-            else:
-                find()
+        find()
+        #get_mask=input("Please Enter Mask :")
+        #range_max_input=int(input("Please Enter Range Max : "))
+        #range_min_input=int(input("Please Enter Range Min : "))
+        #if get_mask.find("192.")!=-1 and get_mask.find("168.")!=-1:
+         #   if get_mask[-1]!=".":
+          #      get_mask=get_mask+"."
+           # if range_max_input>range_min_input and range_max_input<256:
+            #    find(mask=get_mask,mode="manual",range_max=range_max_input,range_min=range_min_input)
+            #else:
+             #   find(mask=get_mask,mode="manual")
+        #else:
+         #   if range_max_input>range_min_input and range_max_input<256:
+          #      find(range_max=range_max_input,mode="manual",range_min=range_min_input)
+           # else:
+                
         
         
         
