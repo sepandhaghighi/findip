e0b527fe897c9568177480a2d0cef800a0581aba
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-01-10 03:37:26 +0330

    fix : modify in logging system

--- ip_finder.py
+++ ip_finder.py
@@ -5,6 +5,15 @@ import datetime
 import sys # For Exit
 import multiprocessing as mu # for multtiprocessing
 import time
+
+
+def line(number,char="-"):
+    response=""
+    i=0
+    while(i<number):
+        response+=char
+        i+=1
+    return response
 def ping(i): # ping function
     output=str(list(sub.Popen("ping "+i,stdout=sub.PIPE,stderr=sub.PIPE,shell=True).communicate())[0])
     return output
@@ -31,14 +40,21 @@ def search_ip(output): # This Function Get A String As Input ( ARP Command Outpu
     return ip_list
 def string_conv(i):
     return mask+str(i)
-def ARP(my_ip):
-    sub.Popen("ping " + my_ip, stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
-    response = sub.Popen("arp -a", stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
-    output = str(list(response.communicate())[0])
-    ip_list = search_ip(output)
-    ip_list = ip_filter(ip_list)
-    for i in ip_list:
-        print("IP : ", i, "Is  available")
+def ARP(my_ip,file):
+    try:
+        sub.Popen("ping " + my_ip, stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
+        response = sub.Popen("arp -a", stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
+        output = str(list(response.communicate())[0])
+        ip_list = search_ip(output)
+        ip_list = ip_filter(ip_list)
+        for i in ip_list:
+            print("IP : ", i, "Is  available")
+            file.write("IP : "+ i+ "Is  available\n")
+    except Exception as e:
+        if file.closed==False:
+            file.close()
+        print(e)
+
 def Manual(range_min,range_max,file):
     try:
         ip_list = list(map(string_conv, list(range(range_min, range_max + 1))))
@@ -59,11 +75,14 @@ def Manual(range_min,range_max,file):
 
 def find(mode="manual",my_ip="0.0.0.0",range_min=0,range_max=254): # This Function Ping And SSH IPs to find SSH Server
     log_file=open("log_file.txt","a")
+    log_file.write(str(datetime.datetime.now())+"\n")
+    log_file.write(line(30,"%")+"\n")
     if mode=="manual":
         Manual(range_max=range_max,range_min=range_min,file=log_file)
-        log_file.close()
     elif mode=="ARP":
-        ARP(my_ip)
+        ARP(my_ip,log_file)
+    log_file.write(line(30, "*") + "\n")
+    log_file.close()
 
 if __name__=="__main__":
     mask="192.168.166."
